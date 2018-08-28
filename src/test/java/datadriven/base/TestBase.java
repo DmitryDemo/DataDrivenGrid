2b1c665ad93ea99e75aa7f1677cbbe3d18ee7a90
DmitryDemo <forregistration@live.ru>
2018-08-28 22:48:45 +0300

    Created TestBase

---
+++ src/test/java/datadriven/base/TestBase.java
@@ -0,0 +1,15 @@
+package datadriven.base;
+
+public class TestBase {
+
+    /*
+    This class is going to initialize:
+    - WebDriver
+    - Logs
+    - Properties
+    - Excel
+    - DataBase
+    - Email
+    - ExtentReport
+     */
+}
